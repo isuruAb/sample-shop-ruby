5207a4ec4d8b931a8dbd3201d56028fc106937e6
isuruAb <vpisabeywardana@gmail.com>
2018-10-17 14:00:06 +0800

    updated user migrate file  and seeder

--- db/migrate/20181017054912_create_users.rb
+++ db/migrate/20181017054912_create_users.rb
@@ -1,7 +1,10 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false, index: {unique: true}
+      t.string :password_digest, null: false
+      t.boolean :isAdmin, null: false, default: false
+      t.datetime :last_login
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+    User.create!(
+        name: Faker::Internet.email, 
+        password:Faker::Code.imei, 
+        last_login: Faker::Time.between(DateTime.now - 1, DateTime.now))
+   end
+User.create!(
+name: "isuru", 
+password:"password", 
+isAdmin: true,
+last_login: Faker::Time.between(DateTime.now - 1, DateTime.now))
+User.create!(
+name: "isuru1", 
+password:"password", 
+isAdmin: false,
+last_login: Faker::Time.between(DateTime.now - 1, DateTime.now))
